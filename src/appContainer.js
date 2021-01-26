f1fd890a0cef1e460584180c4b8e9fc041d01e5d
Dmg5th <dmgould310@gmail.com>
2021-01-26 12:05:51 -0500

    appended artist to DOM with renderArtist method

--- src/appContainer.js
+++ src/appContainer.js
@@ -5,38 +5,46 @@ class AppContainer {
     discoverArtists = {};
 
     bindEventListeners(){
-        const button = document.querySelector("#create-discover")
-        button.addEventListener("click", this.getRandomArtists)
+      const button = document.querySelector("#create-discover")
+      button.addEventListener("click", this.getRandomArtists)
     };
 
     getRandomArtists(){
-        debugger
-       let randomArtists = []
+      let randomArtists = []
         for (let index = 0; index < 4; index++) {
-           
-            //build this out to cover functionality to randomize each era for discovery 
+           //build this out to cover functionality to randomize each era for discovery 
             randomArtists.push(AppContainer.artists[Math.floor(Math.random() * AppContainer.artists.length)]);
         }
-         return randomArtists
+      return randomArtists
     }
     
     getArtists(){
+       //make a fetch request to artists
         fetch(this.url + 'artists')
         .then(resp => resp.json())
          //populate artists and eras properties with the returned data
         .then(data =>  {
-            console.log(data)
+            // console.log(data)
             data.forEach(artist => {
                 new Artist(artist.name, artist.era) 
-            });
+             });
+        this.renderArtists()
         }) 
-        //make a fetch request to artists
-        //then call renderArtists
-        .catch(err => alert(err)); 
-    }
+        .catch(err => alert(err));   
+    };
     renderArtists(){
         //create DOM nodes and the insert data into them 
+        const ul = document.createElement("ul");
+        AppContainer.artists.forEach(artist => {
+            const li = document.createElement("li")
+            li.innerText = artist.name
+            ul.append(li)
+        })
+        document.body.append(ul)
     }
+
+
+
 }
 
 
